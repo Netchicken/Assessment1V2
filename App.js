641f48525efbd151a8cd4f8b6444a3788333043e
Gary <Gary.Dix@enform.nz>
2022-03-15 12:22:48 +1300

    init

---
+++ App.js
@@ -0,0 +1,154 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+// import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  SectionList,
+  Colors,
+  Button,
+} from 'react-native';
+
+import {LoadAllCities} from './Operations';
+import {countryData} from './cities';
+
+const App = () => {
+  const [allData, setAllData] = useState(countryData);
+  const [gameData, setGameData] = useState({
+    CountryName: 'aa',
+    CapitalName: 'aa',
+    CapitalLatitude: 0,
+    CapitalLongitude: 0,
+    ContinentName: 'aa',
+  });
+
+  const [number, setNumber] = useState(0);
+
+  const increase = () => {
+    var datalength = allData.length;
+    var randomNumber = getRandomNumberBetween(0, datalength);
+
+    number == 14 ? setNumber(1) : setNumber(number + 1);
+  };
+
+  const getRandomNumberBetween = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const Section = ({children, title}) => {
+    return (
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>
+          {title} {number}
+        </Text>
+        <Text>
+          the city is {gameData.CapitalName ? gameData.CapitalName : ''}{' '}
+        </Text>
+        <Text>
+          the Country is {gameData.CountryName ? gameData.CountryName : ''}
+        </Text>
+        <Text>
+          the Continent is{' '}
+          {gameData.ContinentName ? gameData.ContinentName : ''}
+        </Text>
+      </View>
+    );
+  };
+
+  const sampleData = [
+    {
+      CountryName: 'Somaliland',
+      CapitalName: 'Hargeisa',
+      CapitalLatitude: 9.55,
+      CapitalLongitude: 44.05,
+      ContinentName: 'Africa',
+    },
+    {
+      CountryName: 'South Georgia and South Sandwich Islands',
+      CapitalName: 'King Edward Point',
+      CapitalLatitude: -54.283333,
+      CapitalLongitude: -36.5,
+      ContinentName: 'Antarctica',
+    },
+    {
+      CountryName: 'French Southern and Antarctic Lands',
+      CapitalName: 'Port-aux-Français',
+      CapitalLatitude: -49.35,
+      CapitalLongitude: 70.216667,
+      ContinentName: 'Antarctica',
+    },
+    {
+      CountryName: 'Palestine',
+      CapitalName: 'Jerusalem',
+      CapitalLatitude: 31.76666667,
+      CapitalLongitude: 35.233333,
+      ContinentName: 'Asia',
+    },
+  ];
+
+  const onClickHandler = () => {
+    increase(); //increase the counter
+    allData.map((item, id) => {
+      var selecteditem = allData[number]; //get the data at that point
+
+      setGameData({
+        CountryName: selecteditem.CountryName,
+        CapitalName: selecteditem.CapitalName,
+        CapitalLatitude: selecteditem.CapitalLatitude,
+        CapitalLongitude: selecteditem.CapitalLongitude,
+        ContinentName: selecteditem.ContinentName,
+      });
+    });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Section
+        style={styles.sectionTitle}
+        title="Guess the Capital City"></Section>
+
+      <Button title="New Country" onPress={onClickHandler}></Button>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: '#0080ff',
+    marginHorizontal: 20,
+  },
+});
+
+export default App;
---
+++ Operations.js
@@ -0,0 +1,5 @@
+import countryData from './cities';
+
+export async function LoadAllCities() {
+  return await countryData;
+}
